99917bd683c38a49b332a34c4ee828e1948fa837
pcast <pcast@DESK-E>
2023-09-08 15:37:34 -0600

    ...

--- src/labeditordetexto/editor.java
+++ src/labeditordetexto/editor.java
@@ -20,6 +20,7 @@ import javax.swing.DefaultComboBoxModel;
 import javax.swing.JColorChooser;
 import javax.swing.JComboBox;
 import javax.swing.JFileChooser;
+import javax.swing.JOptionPane;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 import javax.swing.filechooser.FileNameExtensionFilter;
@@ -511,27 +512,34 @@ public class editor extends javax.swing.JFrame {
     }//GEN-LAST:event_abrirArchivoMouseClicked
 
     private void crearArchivoMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_crearArchivoMouseClicked
-        JFileChooser fileChooser = new JFileChooser();
-        fileChooser.setCurrentDirectory(new File("."));
+        if(vis.getText().isBlank() || vis.getText().isEmpty() ){
+            JOptionPane.showMessageDialog(null, "NO HA INGRESADO TEXTO");
+        }else{
 
-        int response = fileChooser.showSaveDialog(null);
+            JFileChooser fileChooser = new JFileChooser();
+            fileChooser.setCurrentDirectory(new File("."));
 
-        if(response == JFileChooser.APPROVE_OPTION) {
-            File file;
-            PrintWriter fileOut = null;
+            int response = fileChooser.showSaveDialog(null);
 
-            file = new File(fileChooser.getSelectedFile().getAbsolutePath());
-            try {
-                fileOut = new PrintWriter(file);
-                fileOut.println(vis.getText());
-                vis.setText("");
-            } 
-            catch (FileNotFoundException e1) {
-                e1.printStackTrace();
+            if(response == JFileChooser.APPROVE_OPTION) {
+                File file;
+                PrintWriter fileOut = null;
+
+                file = new File(fileChooser.getSelectedFile().getAbsolutePath());
+                try {
+                    fileOut = new PrintWriter(file);
+                    fileOut.println(vis.getText());
+                    vis.setText("");
+                    JOptionPane.showMessageDialog(null, "GUARDADO");
+
+                } 
+                catch (FileNotFoundException e1) {
+                    e1.printStackTrace();
+                }
+                finally {
+                    fileOut.close();
+                }   
             }
-            finally {
-                fileOut.close();
-            }   
         }
     }//GEN-LAST:event_crearArchivoMouseClicked
 
@@ -539,37 +547,7 @@ public class editor extends javax.swing.JFrame {
     /**
      * @param args the command line arguments
      */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(editor.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(editor.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(editor.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(editor.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new editor().setVisible(true);
-            }
-        });
-    }
+    
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton abrirArchivo;
